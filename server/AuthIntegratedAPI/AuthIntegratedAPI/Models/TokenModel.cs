c8defb454ce6ce0c402a69664cc9139fd39e0405
Sreejith Sasidharan <sreejith.sasidharan@experionglobal.com>
2024-07-05 11:23:42 +0530

    Initial commit

---
+++ server/AuthIntegratedAPI/AuthIntegratedAPI/Models/TokenModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthIntegratedAPI.Models
+{
+    public class TokenModel
+    {
+        public string accessToken { get; set; }
+    }
+}
